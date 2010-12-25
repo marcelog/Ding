f617a861442f3f3fbab56dca6a8f5491df6f52d4
Marcelo Gornstein <marcelog@gmail.com>
2010-12-24 23:09:12 -0300

    added test bootstrap code

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,1 +1,13 @@
 <?php
+ini_set(
+    'include_path',
+    implode(
+        PATH_SEPARATOR,
+        array(
+            ini_get('include_path'),
+            implode(DIRECTORY_SEPARATOR, array('..', 'src', 'mg'))
+        )
+    )
+);
+require_once 'Ding/Autoloader/Autoloader.php'; // Include ding autoloader.
+Autoloader::register(); // Call autoloader register for ding autoloader.
