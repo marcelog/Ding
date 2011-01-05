ea039d8ebe24a64353e2984a63c1a9b9e4ef0e67
Marcelo Gornstein <marcelog@gmail.com>
2011-01-05 14:05:59 -0300

    added doc

--- src/mg/Ding/MVC/Http/HttpFrontController.php
+++ src/mg/Ding/MVC/Http/HttpFrontController.php
@@ -1,14 +1,50 @@
 <?php
+/**
+ * Http front controller.
+ *
+ * PHP Version 5
+ *
+ * @category   Ding
+ * @package    Mvc
+ * @subpackage Http
+ * @author     Marcelo Gornstein <marcelog@gmail.com>
+ * @license    http://www.noneyet.ar/ Apache License 2.0
+ * @version    SVN: $Id$
+ * @link       http://www.noneyet.ar/
+ */
 namespace Ding\MVC\Http;
 
 use Ding\MVC\ModelAndView;
-
 use Ding\Container\Impl\ContainerImpl;
 
+/**
+ * Http front controller.
+ *
+ * PHP Version 5
+ *
+ * @category   Ding
+ * @package    Mvc
+ * @subpackage Http
+ * @author     Marcelo Gornstein <marcelog@gmail.com>
+ * @license    http://www.noneyet.ar/ Apache License 2.0
+ * @link       http://www.noneyet.ar/
+ */
 class HttpFrontController
 {
+    /**
+     * Container properties.
+     * @var array
+     */
     private static $_properties;
-    
+
+    /**
+     * Handles the request. This will instantiate the container with the given
+     * properties (via static method configure(), see below). Then it will
+     * getBean(HttpDispatcher) and call dispatch() on it with an Action created
+     * based on the request uri and method parameters (get, post, etc).
+     *
+     * @return void
+     */
     public function handle()
     {
         try
@@ -24,7 +60,7 @@ class HttpFrontController
                 $vars = $_GET;
             } else if ($method === 'POST') {
                 $vars = $_POST;
-            }                
+            }
             foreach ($_GET as $k => $v) {
                 $arguments[$method][$k] = $v;
             }
@@ -35,12 +71,24 @@ class HttpFrontController
             header('HTTP/1.1 500 Not Found');
         }
     }
-    
-    public static function configure($properties)
+
+    /**
+     * Configures this frontcontroller with the container properties.
+     *
+     * @param array $properties Container properties.
+     *
+     * @return void
+     */
+    public static function configure(array $properties)
     {
         self::$_properties = $properties;
     }
 
+    /**
+     * Constructor.
+     *
+     * @return void
+     */
     public function __construct()
     {
     }
