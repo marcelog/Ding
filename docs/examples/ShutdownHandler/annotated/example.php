2150f8a45ca4bac59599d2a963d2ffa8689c2ec8
Marcelo Gornstein <marcelog@gmail.com>
2011-01-19 23:08:25 -0300

    shutdown handler example added

---
+++ docs/examples/ShutdownHandler/annotated/example.php
@@ -0,0 +1,44 @@
+<?php
+declare(ticks=1);
+/**
+ * Example using annotated SignalHandler.
+ *
+ * PHP Version 5
+ *
+ * @category   Ding
+ * @package    Examples
+ * @subpackage SignalHandler.Annotated
+ * @author     Marcelo Gornstein <marcelog@gmail.com>
+ * @license    http://www.noneyet.ar/ Apache License 2.0
+ * @link       http://www.noneyet.ar/
+ */
+require_once 'Ding/Autoloader/Ding_Autoloader.php'; // Include ding autoloader.
+Ding_Autoloader::register(); // Call autoloader register for ding autoloader.
+use Ding\Container\Impl\ContainerImpl;
+
+error_reporting(E_ALL);
+ini_set('display_errorrs', 1);
+
+// Here you configure the container, its subcomponents, drivers, etc.
+$properties = array(
+    'ding' => array(
+        'log4php.properties' => './log4php.properties',
+        'factory' => array(
+            'bdef' => array( // Both of these drivers are optional. They are both included just for the thrill of it.
+                'annotation' => array('scanDir' => array(realpath(__DIR__)))
+            ),
+            // These properties will be used by the container when instantiating the beans, see beans.xml
+            'properties' => array(
+                'user.name' => 'nobody',
+            )
+        ),
+        // You can configure the cache for the bean definition, the beans, and the proxy definitions.
+        // Other available implementations: zend, file, dummy, and memcached.
+    	'cache' => array(
+            'proxy' => array('impl' => 'dummy'),
+            'bdef' => array('impl' => 'dummy'),
+            'beans' => array('impl' => 'dummy')
+        )
+    )
+);
+$container = ContainerImpl::getInstance($properties);
---
+++ docs/examples/ShutdownHandler/annotated/handler.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Example using annotated SignalHandler.
+ *
+ * PHP Version 5
+ *
+ * @category   Ding
+ * @package    Examples
+ * @subpackage ShutdownHandler.Annotated
+ * @author     Marcelo Gornstein <marcelog@gmail.com>
+ * @license    http://www.noneyet.ar/ Apache License 2.0
+ * @link       http://www.noneyet.ar/
+ */
+use Ding\Helpers\ShutdownHandler\IShutdownHandler;
+
+/**
+ * @ShutdownHandler
+ */
+class MyShutdownHandler implements IShutdownHandler
+{
+    public function handleShutdown()
+    {
+        echo "This is your custom shutdown handler\n";
+    }
+
+    public function __construct()
+    {
+    }
+}
