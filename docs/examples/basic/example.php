49f7ce78d69ae00e1b3c130dda60b9687b82ce31
Marcelo Gornstein <marcelog@gmail.com>
2011-01-19 21:44:39 -0300

    added basic example

---
+++ docs/examples/basic/example.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Example using ding. See also beans.xml.
+ *
+ * PHP Version 5
+ *
+ * @category   Ding
+ * @package    Examples
+ * @subpackage Basic
+ * @author     Marcelo Gornstein <marcelog@gmail.com>
+ * @license    http://www.noneyet.ar/ Apache License 2.0
+ * @link       http://www.noneyet.ar/
+ */
+
+/**
+ * This is our bean.
+ */
+class MyBean
+{
+    private $_someProperty;
+
+    public function setSomeProperty($value)
+    {
+        $this->_someProperty = $value;
+    }
+
+    public function getSomeProperty()
+    {
+        return $this->_someProperty;
+    }
+    public function __construct()
+    {
+
+    }
+}
+require_once 'Ding/Autoloader/Ding_Autoloader.php'; // Include ding autoloader.
+Ding_Autoloader::register(); // Call autoloader register for ding autoloader.
+use Ding\Container\Impl\ContainerImpl;
+
+// Here you configure the container, its subcomponents, drivers, etc.
+$properties = array(
+    'ding' => array(
+        'log4php.properties' => './log4php.properties',
+        'factory' => array(
+            'bdef' => array( // Both of these drivers are optional. They are both included just for the thrill of it.
+                'xml' => array('filename' => 'beans.xml'),
+                'annotation' => array('scanDir' => array(realpath(__DIR__)))
+            ),
+            // These properties will be used by the container when instantiating the beans, see beans.xml
+            'properties' => array(
+                'user.name' => 'nobody',
+                'timezone' => 'America/Buenos_Aires'
+            )
+        ),
+        // You can configure the cache for the bean definition, the beans, and the proxy definitions.
+        // Other available implementations: zend, file, dummy, and memcached.
+    	'cache' => array(
+            'proxy' => array('impl' => 'apc'),
+            'bdef' => array('impl' => 'apc'),
+            'beans' => array('impl' => 'apc')
+        )
+    )
+);
+$container = ContainerImpl::getInstance($properties);
+$bean = $container->getBean('myBeanName');
+var_dump($bean->getSomeProperty());
