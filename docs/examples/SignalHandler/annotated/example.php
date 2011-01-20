c6f3c1ee63db7bff8215f79c92175c4b65ba6074
Marcelo Gornstein <marcelog@gmail.com>
2011-01-19 22:48:25 -0300

    added signal handler helper example, with and without annotations

--- docs/examples/SignalHandler/annotated/example.php
+++ docs/examples/SignalHandler/annotated/example.php
@@ -1,41 +1,25 @@
 <?php
+declare(ticks=1);
 /**
- * Example using ding. See also beans.xml.
+ * Example using annotated SignalHandler.
  *
  * PHP Version 5
  *
  * @category   Ding
  * @package    Examples
- * @subpackage Basic
+ * @subpackage SignalHandler.Annotated
  * @author     Marcelo Gornstein <marcelog@gmail.com>
  * @license    http://www.noneyet.ar/ Apache License 2.0
  * @link       http://www.noneyet.ar/
  */
-
-/**
- * This is our bean.
- */
-class MyBean
-{
-    private $_someProperty;
-
-    public function setSomeProperty($value)
-    {
-        $this->_someProperty = $value;
-    }
-
-    public function getSomeProperty()
-    {
-        return $this->_someProperty;
-    }
-    public function __construct()
-    {
-
-    }
-}
 require_once 'Ding/Autoloader/Ding_Autoloader.php'; // Include ding autoloader.
 Ding_Autoloader::register(); // Call autoloader register for ding autoloader.
 use Ding\Container\Impl\ContainerImpl;
+use Ding\Bean\Factory\Driver\SignalHandlerDriver;
+use Ding\Helpers\SignalHandler\ISignalHandler;
+
+error_reporting(E_ALL);
+ini_set('display_errorrs', 1);
 
 // Here you configure the container, its subcomponents, drivers, etc.
 $properties = array(
@@ -43,7 +27,6 @@ $properties = array(
         'log4php.properties' => './log4php.properties',
         'factory' => array(
             'bdef' => array( // Both of these drivers are optional. They are both included just for the thrill of it.
-                'xml' => array('filename' => 'beans.xml'),
                 'annotation' => array('scanDir' => array(realpath(__DIR__)))
             ),
             // These properties will be used by the container when instantiating the beans, see beans.xml
@@ -54,12 +37,12 @@ $properties = array(
         // You can configure the cache for the bean definition, the beans, and the proxy definitions.
         // Other available implementations: zend, file, dummy, and memcached.
     	'cache' => array(
-            'proxy' => array('impl' => 'apc'),
-            'bdef' => array('impl' => 'apc'),
-            'beans' => array('impl' => 'apc')
+            'proxy' => array('impl' => 'dummy'),
+            'bdef' => array('impl' => 'dummy'),
+            'beans' => array('impl' => 'dummy')
         )
     )
 );
 $container = ContainerImpl::getInstance($properties);
-$bean = $container->getBean('myBeanName');
-var_dump($bean->getSomeProperty());
+posix_kill(posix_getpid(), SIGHUP);
+sleep(1);
--- docs/examples/SignalHandler/example.php
+++ docs/examples/SignalHandler/example.php
@@ -1,41 +1,41 @@
 <?php
+declare(ticks=1);
+
 /**
- * Example using ding. See also beans.xml.
+ * Example using annotated SignalHandler.
  *
  * PHP Version 5
  *
  * @category   Ding
  * @package    Examples
- * @subpackage Basic
+ * @subpackage SignalHandler
  * @author     Marcelo Gornstein <marcelog@gmail.com>
  * @license    http://www.noneyet.ar/ Apache License 2.0
  * @link       http://www.noneyet.ar/
  */
+require_once 'Ding/Autoloader/Ding_Autoloader.php'; // Include ding autoloader.
+Ding_Autoloader::register(); // Call autoloader register for ding autoloader.
+use Ding\Container\Impl\ContainerImpl;
+use Ding\Bean\Factory\Driver\SignalHandlerDriver;
+use Ding\Helpers\SignalHandler\ISignalHandler;
+
+error_reporting(E_ALL);
+ini_set('display_errorrs', 1);
 
 /**
  * This is our bean.
  */
-class MyBean
+class MySignalHandler implements ISignalHandler
 {
-    private $_someProperty;
-
-    public function setSomeProperty($value)
+    public function handleSignal($signal)
     {
-        $this->_someProperty = $value;
+        echo "This is your custom signal handler: " . $signal . "\n";
     }
 
-    public function getSomeProperty()
-    {
-        return $this->_someProperty;
-    }
     public function __construct()
     {
-
     }
 }
-require_once 'Ding/Autoloader/Ding_Autoloader.php'; // Include ding autoloader.
-Ding_Autoloader::register(); // Call autoloader register for ding autoloader.
-use Ding\Container\Impl\ContainerImpl;
 
 // Here you configure the container, its subcomponents, drivers, etc.
 $properties = array(
@@ -44,7 +44,6 @@ $properties = array(
         'factory' => array(
             'bdef' => array( // Both of these drivers are optional. They are both included just for the thrill of it.
                 'xml' => array('filename' => 'beans.xml'),
-                'annotation' => array('scanDir' => array(realpath(__DIR__)))
             ),
             // These properties will be used by the container when instantiating the beans, see beans.xml
             'properties' => array(
@@ -54,12 +53,12 @@ $properties = array(
         // You can configure the cache for the bean definition, the beans, and the proxy definitions.
         // Other available implementations: zend, file, dummy, and memcached.
     	'cache' => array(
-            'proxy' => array('impl' => 'apc'),
-            'bdef' => array('impl' => 'apc'),
-            'beans' => array('impl' => 'apc')
+            'proxy' => array('impl' => 'dummy'),
+            'bdef' => array('impl' => 'dummy'),
+            'beans' => array('impl' => 'dummy')
         )
     )
 );
 $container = ContainerImpl::getInstance($properties);
-$bean = $container->getBean('myBeanName');
-var_dump($bean->getSomeProperty());
+posix_kill(posix_getpid(), SIGHUP);
+sleep(1);
