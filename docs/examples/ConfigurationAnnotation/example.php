8b092cb26912e6c1d398e158414e42356251184c
Marcelo Gornstein <marcelog@gmail.com>
2011-01-22 13:59:32 -0300

    simplified config annotation example

--- docs/examples/ConfigurationAnnotation/example.php
+++ docs/examples/ConfigurationAnnotation/example.php
@@ -23,6 +23,25 @@ ini_set(
 );
 
 /**
+ * @Configuration
+ */
+class SomeBeanProviderClass
+{
+    /**
+     * @Bean
+     * @Scope(value='prototype')
+     * @InitMethod(method=aMethod)
+     * @DestroyMethod(method=bMethod)
+     */
+    public function someBean()
+    {
+        $ret = new MyBean();
+        $ret->setSomeProperty('hello world');
+        return $ret;
+    }
+}
+
+/**
  * This is our bean.
  * @author SomeAuthor
  */
