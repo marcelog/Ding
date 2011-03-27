11a65ad9a892a6b5ef6e1ceb404ff9f3a6df523b
Marcelo Gornstein <marcelog@gmail.com>
2011-03-27 16:30:39 -0300

    some unit tests for annotations driver

---
+++ test/ioc/annotation/Test_Annotation_IoC.php
@@ -0,0 +1,210 @@
+<?php
+/**
+ * This class will test the annotation driver.
+ *
+ * PHP Version 5
+ *
+ * @category   Ding
+ * @package    Test
+ * @subpackage Ioc.Annotation
+ * @author     Marcelo Gornstein <marcelog@gmail.com>
+ * @license    http://marcelog.github.com/ Apache License 2.0
+ * @version    SVN: $Id$
+ * @link       http://marcelog.github.com/
+ *
+ * Copyright 2011 Marcelo Gornstein <marcelog@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+use Ding\Container\Impl\ContainerImpl;
+
+/**
+ * This class will test the annotation driver.
+ *
+ * PHP Version 5
+ *
+ * @category   Ding
+ * @package    Test
+ * @subpackage Ioc.Annotation
+ * @author     Marcelo Gornstein <marcelog@gmail.com>
+ * @license    http://marcelog.github.com/ Apache License 2.0
+ * @link       http://marcelog.github.com/
+ */
+class Test_Annotation_IoC extends PHPUnit_Framework_TestCase
+{
+    private $_properties = array();
+
+    public function setUp()
+    {
+        $this->_properties = array(
+            'ding' => array(
+                'log4php.properties' => RESOURCES_DIR . DIRECTORY_SEPARATOR . 'log4php.properties',
+                'factory' => array(
+                    'bdef' => array(
+                        'annotation' => array(
+                        	'scanDir' => array(realpath(__DIR__))
+                        )
+                    )
+                )
+            )
+        );
+    }
+    /**
+     * @test
+     */
+    public function can_singleton()
+    {
+        $container = ContainerImpl::getInstance($this->_properties);
+        $bean = $container->getBean('aSimpleSingletonBean');
+        $bean2 = $container->getBean('aSimpleSingletonBean');
+
+        $this->assertTrue($bean instanceof ClassSimpleAnnotation);
+        $this->assertTrue($bean2 instanceof ClassSimpleAnnotation);
+
+        // If this is truly a singleton, both "sets" will go to the same
+        // object reference.
+        $bean->setSomething(rand(1, microtime(true)));
+        $bean2->setSomething(rand(1, microtime(true)));
+        $this->assertTrue($bean->getSomething() === $bean2->getSomething());
+    }
+
+    /**
+     * @test
+     */
+    public function can_prototype()
+    {
+        $container = ContainerImpl::getInstance($this->_properties);
+        $bean = $container->getBean('aSimplePrototypeBean');
+        $bean2 = $container->getBean('aSimplePrototypeBean');
+
+        $this->assertTrue($bean instanceof ClassSimpleAnnotation);
+        $this->assertTrue($bean2 instanceof ClassSimpleAnnotation);
+
+        // Contrary to what happens with singletons, in this case both "sets"
+        // will go to different instances.
+        $bean->setSomething(rand(1, microtime(true)));
+        $bean2->setSomething(rand(1, microtime(true)));
+        $this->assertFalse($bean->getSomething() === $bean2->getSomething());
+    }
+
+    /**
+     * @test
+     */
+    public function can_init_method()
+    {
+        $container = ContainerImpl::getInstance($this->_properties);
+        $bean = $container->getBean('aSimpleInitMethodBean');
+        $this->assertTrue($bean->something);
+    }
+
+    /**
+     * @test
+     */
+    public function can_destroy_method()
+    {
+        $container = ContainerImpl::getInstance($this->_properties);
+        $bean = $container->getBean('aSimpleDestroyMethodBean');
+         // XXX bad... unset() does not work because ContainerImpl is a singleton
+         // and holds a reference to itself, so the destructor is never called.
+        $container->__destruct();
+        $this->assertNull($bean->something);
+    }
+}
+
+/**
+ * @Configuration
+ */
+class ClassSimpleAnnotationConfiguration
+{
+    /**
+     * @Bean(class=ClassSimpleAnnotation)
+     * @Scope(value=singleton)
+     * @InitMethod(method=initMethod)
+     */
+    public function aSimpleInitMethodBean()
+    {
+        return new ClassSimpleAnnotation4();
+    }
+
+    /**
+     * @Bean(class=ClassSimpleAnnotation)
+     * @Scope(value=singleton)
+     * @DestroyMethod(method=destroyMethod)
+     */
+    public function aSimpleDestroyMethodBean()
+    {
+        return new ClassSimpleAnnotation4();
+    }
+
+    /**
+     * @Bean(class=ClassSimpleAnnotation)
+     * @Scope(value=singleton)
+     */
+    public function aSimpleSingletonBean()
+    {
+        return new ClassSimpleAnnotation();
+    }
+
+    /**
+     * @Bean(class=ClassSimpleAnnotation)
+     * @Scope(value=prototype)
+     */
+    public function aSimplePrototypeBean()
+    {
+        return new ClassSimpleAnnotation();
+    }
+
+    public function __construct()
+    {
+    }
+}
+
+class ClassSimpleAnnotation
+{
+    private $_something;
+
+    public function getSomething()
+    {
+        return $this->_something;
+    }
+
+    public function setSomething($v)
+    {
+        $this->_something = $v;
+    }
+
+    public function __construct()
+    {
+    }
+}
+
+class ClassSimpleAnnotation4
+{
+    public $something = false;
+
+    public function initMethod()
+    {
+        $this->something = true;
+    }
+
+    public function destroyMethod()
+    {
+        $this->something = null;
+    }
+
+    public function __construct()
+    {
+    }
+}
