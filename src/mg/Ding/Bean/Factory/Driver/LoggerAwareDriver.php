9a5ea2ce480777a29b1723165ce0bb5c47c29d83
Marcelo Gornstein <marcelog@gmail.com>
2011-04-21 21:36:19 -0300

    really added LoggerAwareDriver this time

---
+++ src/mg/Ding/Bean/Factory/Driver/LoggerAwareDriver.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * This driver will make the injection of the logger in use by the container
+ * for beans that implement IContainerAware.
+ *
+ * PHP Version 5
+ *
+ * @category   Ding
+ * @package    Bean
+ * @subpackage Factory.Driver
+ * @author     Marcelo Gornstein <marcelog@gmail.com>
+ * @license    http://marcelog.github.com/ Apache License 2.0
+ * @version    SVN: $Id$
+ * @link       http://marcelog.github.com/
+ *
+ * Copyright 2011 Marcelo Gornstein <marcelog@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+namespace Ding\Bean\Factory\Driver;
+
+use Ding\Bean\BeanPropertyDefinition;
+use Ding\Bean\Lifecycle\IAfterCreateListener;
+use Ding\Bean\BeanDefinition;
+use Ding\Bean\BeanAnnotationDefinition;
+use Ding\Bean\Factory\IBeanFactory;
+use Ding\Reflection\ReflectionFactory;
+/**
+ * This driver will make the injection of the logger in use by the container
+ * for beans that implement IContainerAware.
+ *
+ * PHP Version 5
+ *
+ * @category   Ding
+ * @package    Bean
+ * @subpackage Factory.Driver
+ * @author     Marcelo Gornstein <marcelog@gmail.com>
+ * @license    http://marcelog.github.com/ Apache License 2.0
+ * @link       http://marcelog.github.com/
+ */
+class LoggerAwareDriver implements IAfterCreateListener
+{
+    /**
+     * Holds current instance.
+     * @var ContainerAwareDriver
+     */
+    private static $_instance = false;
+
+    /**
+     * (non-PHPdoc)
+     * @see Ding\Bean\Lifecycle.IAfterCreateListener::afterCreate()
+     */
+    public function afterCreate(IBeanFactory $factory, $bean, BeanDefinition $beanDefinition)
+    {
+        $class = $beanDefinition->getClass();
+        if ($class === false || empty($class)) {
+            return $bean;
+        }
+        $rClass = ReflectionFactory::getClass($class);
+        if ($rClass->implementsInterface('Ding\Logger\ILoggerAware')) {
+            $bean->setLogger($factory->getLogger($class));
+        }
+        return $bean;
+    }
+
+    /**
+     * Returns an instance.
+     *
+     * @param array $options Optional options.
+     *
+     * @return LoggerAwareDriver
+     */
+    public static function getInstance(array $options)
+    {
+        if (self::$_instance == false) {
+            self::$_instance = new LoggerAwareDriver;
+        }
+        return self::$_instance;
+    }
+
+    /**
+     * Constructor.
+     *
+     * @return void
+     */
+    private function __construct()
+    {
+    }
+}
