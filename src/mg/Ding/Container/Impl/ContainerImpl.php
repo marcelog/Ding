e7415d3580a5b3b101b88fd5ac0cdce2d52069f9
Marcelo Gornstein <marcelog@gmail.com>
2011-12-17 21:01:35 -0300

    fixed trying to resolve twice a property when search and replacing

--- src/mg/Ding/Container/Impl/ContainerImpl.php
+++ src/mg/Ding/Container/Impl/ContainerImpl.php
@@ -289,6 +289,7 @@ class ContainerImpl implements IContainer
                     } else {
                         $value = $v;
                     }
+                    break;
                 }
             }
         }
