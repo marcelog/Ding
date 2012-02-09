99172aa6547761b94d25642928b1338276c66095
Marcelo Gornstein <marcelog@gmail.com>
2012-02-09 11:24:54 -0300

    fixed phar autoloader

--- resources/generatePhar.php
+++ resources/generatePhar.php
@@ -2,6 +2,13 @@
 $stub =
 '<?php
 Phar::mapPhar();
+spl_autoload_register(function ($class) {
+    $classFile = "phar://ding.phar/" . str_replace("\\\", "/", $class) . ".php";
+    if (file_exists($classFile)) {
+        require_once $classFile;
+        return true;
+    }
+});
 include "phar://ding.phar/Ding/Autoloader/Autoloader.php";
 \Ding\Autoloader\Autoloader::register();
 __HALT_COMPILER();
